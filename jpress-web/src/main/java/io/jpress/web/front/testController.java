f3ea675f527b3c7f471e5d33b3a6dd26b6d2b4c4
sixther <382998946@qq.com>
2019-08-21 22:46:07 +0800

    first commit

---
+++ jpress-web/src/main/java/io/jpress/web/front/testController.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.web.front;
+
+import io.jboot.utils.StrUtil;
+import io.jboot.web.controller.annotation.RequestMapping;
+import io.jpress.JPressOptions;
+import io.jpress.web.base.TemplateControllerBase;
+import io.jpress.web.handler.JPressHandler;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Package io.jpress.web
+ */
+@RequestMapping("/test")
+public class testController extends TemplateControllerBase {
+
+
+    public void index() {
+
+
+        String indexView = StrUtil.isBlank(JPressOptions.getIndexStyle())
+                ? "index.html"
+                : "index_" + JPressOptions.getIndexStyle() + ".html";
+
+
+        //渲染 模板下的 index.html
+        render(indexView);
+    }
+
+}
